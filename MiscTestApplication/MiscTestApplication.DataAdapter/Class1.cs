aaa442711029dd57cb32b5c21648f66016f497e5
andregravato <andre.gravato@wonga.com>
2012-10-04 12:54:35 +0100

    test1

---
+++ MiscTestApplication/MiscTestApplication.DataAdapter/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiscTestApplication.DataAdapter
+{
+    public class Class1
+    {
+    }
+}
---
+++ MiscTestApplication/MiscTestApplication/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using MiscTestApplication.DataAdapter;
+
+namespace MiscTestApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EntityModelContainer container = new EntityModelContainer();
+
+            MessageBox.Show(container.MainEntity1.First().Name);
+
+        }
+    }
+}
